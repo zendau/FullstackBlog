fc24b6b9a971553ae2776482168d504bb6f7ad2d
Zendau <cowalyow2012@gmail.com>
2023-12-06 23:40:33 +0300

    refactor(server): Change the entry point to the app class

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` })
 
 const express = require("express")
-const app = express()
 const path = require("path")
 
 const swaggerUI = require("swagger-ui-express")
@@ -12,6 +11,8 @@ const cors = require("cors")
 
 const mongoose = require("mongoose")
 
+const { cleanEnv, str, port, num } = require("envalid")
+
 const userRoute = require("./routes/user.route")
 const postRoute = require("./routes/post.route")
 const fileRoute = require("./routes/file.route")
@@ -20,91 +21,115 @@ const adminRoute = require("./routes/admin.route")
 
 const errorMiddleware = require("./middlewares/error.middleware")
 
-const PORT = process.env.PORT || 3000
+class App {
+  constructor(port) {
+    this.app = express()
+    this.port = port
 
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Blog API",
-      version: "1.0.0",
-      description: "A simple Express Blog API",
-    },
-  },
-  apis: ["./routes/*.js", "./models/*.js", "./dtos/*.js"],
-}
+    this.validateEnv()
 
-const specs = swaggerJsDoc(options)
+    this.initializeMiddlewares()
+    this.initializeRoutes()
+    this.initializeErrorHandling()
+  }
 
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs))
-
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-
-app.use(cookieParser())
-
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.CLIENT_URL,
-  }),
-)
-
-app.use("/user", userRoute)
-app.use("/post", postRoute)
-app.use("/file", fileRoute)
-app.use("/comment", commentRoute)
-app.use("/admin", adminRoute)
-
-// Error middleware
-// app.use((err, req, res, next) => {
-//   // Обработка ошибки
-
-//   console.log("2error", err)
-
-//   // eslint-disable-next-line no-debugger
-//   // debugger
-
-//   res.status(err.status || 500)
-//   res.json({
-//     error: {
-//       message: err.message,
-//     },
-//   })
-// })
-
-// app.use((err, req, res, next) => {
-//   console.error(err.stack)
-//   res.status(err.status || 500)
-//   res.json({
-//     error: {
-//       message: err.message,
-//     },
-//   })
-//   // res.status(500).send("Something broke!")
-// })
-
-app.use(errorMiddleware)
-app.use(
-  "/image",
-  express.static(path.join(__dirname, process.env.FILE_FOULDER)),
-)
-
-async function start() {
-  try {
-    await mongoose.connect(process.env.DB_URL, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
+  validateEnv() {
+    cleanEnv(process.env, {
+      PORT: port(),
+      DB_URL: str(),
+      BCRYPT_SALT: num(),
+      JWT_ACCESS_SECRET: str(),
+      JWT_ACCESS_EXPIRES: str(),
+      JWT_REFRESH_SECRET: str(),
+      JWT_REFRESH_EXPIRES: str(),
+      CLIENT_URL: str(),
+      MAILER_HOST: str(),
+      MAILER_PORT: port(),
+      MAILER_USER: str(),
+      MAILER_PASS: str(),
+      MAILER_FROM: str(),
+      FILE_FOULDER: str(),
     })
+  }
 
-    app.listen(PORT, () => {
-      console.log(`server started on http://localhost:${PORT}`)
-    })
-  } catch (e) {
-    console.log("ee", e)
-    console.error(e.message)
+  initializeSwaggerSpecs() {
+    const options = {
+      definition: {
+        openapi: "3.0.0",
+        info: {
+          title: "Blog API",
+          version: "1.0.0",
+          description: "A simple Express Blog API",
+        },
+      },
+      apis: ["./routes/*.js", "./models/*.js", "./dtos/*.js"],
+    }
+
+    const specs = swaggerJsDoc(options)
+    return specs
+  }
+
+  initializeMiddlewares() {
+    this.app.use(
+      "/api-docs",
+      swaggerUI.serve,
+      swaggerUI.setup(this.initializeSwaggerSpecs()),
+    )
+
+    this.app.use(express.urlencoded({ extended: true }))
+    this.app.use(express.json())
+
+    this.app.use(cookieParser())
+
+    this.app.use(
+      cors({
+        credentials: true,
+        origin: process.env.CLIENT_URL,
+      }),
+    )
+
+    this.app.use(
+      "/image",
+      express.static(path.join(__dirname, process.env.FILE_FOULDER)),
+    )
+  }
+
+  initializeRoutes() {
+    this.app.use("/user", userRoute)
+    this.app.use("/post", postRoute)
+    this.app.use("/file", fileRoute)
+    this.app.use("/comment", commentRoute)
+    this.app.use("/admin", adminRoute)
+  }
+
+  initializeErrorHandling() {
+    this.app.use(errorMiddleware)
+  }
+
+  async listen() {
+    try {
+      await this.connectToTheDatabase()
+      this.app.listen(this.port, () => {
+        console.log(`App listening on the port http://localhost:${this.port}`)
+      })
+    } catch (e) {
+      console.error(`Server Error: -> ${e}`)
+    }
+  }
+
+  async connectToTheDatabase() {
+    try {
+      await mongoose.connect(process.env.DB_URL, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+      })
+    } catch (e) {
+      console.error(`DB Error: -> ${e}`)
+    }
   }
 }
 
-start()
+const PORT = process.env.PORT || 3000
+const app = new App(PORT)
+app.listen()
