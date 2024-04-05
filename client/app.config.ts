96e80ad3039aa826e43ccd0a305e02c8763f5517
Zendau <cowalyow2012@gmail.com>
2024-04-05 17:58:02 +0300

    chore(client): change configs eslint, nuxt and app

--- client/app.config.ts
+++ client/app.config.ts
@@ -4,12 +4,7 @@ export default defineAppConfig({
     input: {
       file: {
         padding: {
-          "2xs": "ps-[85px]",
-          xs: "ps-[87px]",
           sm: "ps-[196px]",
-          md: "ps-[98px]",
-          lg: "ps-[100px]",
-          xl: "ps-[109px]",
         },
       },
     },
