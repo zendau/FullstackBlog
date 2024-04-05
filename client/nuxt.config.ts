2b34d58a7d2ba525f36526e77078225df2cf6d1c
Zendau <cowalyow2012@gmail.com>
2024-04-05 23:40:09 +0300

    chore(client): init pinia

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/ui", "@vueuse/nuxt", "@nuxtjs/eslint-module", "@nuxt/image"],
+  modules: [
+    "@nuxt/ui",
+    "@vueuse/nuxt",
+    "@nuxtjs/eslint-module",
+    "@nuxt/image",
+    "@pinia/nuxt",
+  ],
   devtools: { enabled: true },
   colorMode: {
     preference: "light",
