4a7949822112a054e9503f77119629ee493ff52e
Zendau <cowalyow2012@gmail.com>
2024-04-08 19:33:33 +0300

    refactor(client): structure optimization useFetchJWT

--- client/composables/useFetchJWT.ts
+++ client/composables/useFetchJWT.ts
@@ -1,15 +1,28 @@
-export function useFetchJWT<T>(
-  url: string | (() => string),
-  options: {
-    method: "get" | "post" | "put" | "patch" | "delete"
-  },
-) {
+export function useFetchJWT(url: any, options: any) {
   const token = useCookie("token")
-  return $fetch<T>(`https://api.fakestorejson.com/api/v1/${url}`, {
-    ...options,
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token.value}`,
+
+  const fetchJWT = $fetch.create({
+    baseURL: "https://api.fakestorejson.com/api/v1/",
+    retry: 1,
+    retryStatusCodes: [401],
+    onRequest: (ctx) => {
+      ctx.options.headers = new Headers({
+        Authorization: `Bearer ${token.value}`,
+      })
+    },
+    onResponseError: async (ctx) => {
+      if (ctx.response.status === 401) {
+        try {
+          const res: any = await fetchJWT("auth/refresh-token", {
+            method: "post",
+          })
+          token.value = res.access_token
+        } catch (e) {
+          // TODO: logout
+        }
+      }
     },
   })
+
+  return fetchJWT(url, options)
 }
