880fce6ff989c28be6df7aaeee02af64548d7d4b
Zendau <cowalyow2012@gmail.com>
2024-04-21 21:19:47 +0300

    feat(client): comment list and card

--- client/components/comment/card.vue
+++ client/components/comment/card.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts">
-// карточка комментария
+const { author, date, message } = defineProps({
+  author: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+})
 </script>
 
 <template>
-  <div></div>
+  <div class="comment__container">
+    <p>Name: {{ author }}</p>
+    <p>Message: {{ message }}</p>
+    <p>Date: {{ date }}</p>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.comment {
+  &__container {
+    border: 1px solid black;
+    padding: 15px;
+    margin: 10px;
+  }
+}
+</style>
--- client/components/comment/list.vue
+++ client/components/comment/list.vue
@@ -1,9 +1,47 @@
 <script setup lang="ts">
-// список с карточками комментов
+import { vIntersectionObserver } from "@vueuse/components"
+
+const commentStore = useCommentStore()
+
+const { pending } = await useAsyncData(
+  "posts",
+  async () => await commentStore.fetch(),
+  {
+    server: true,
+  },
+)
+
+function onIntersectionObserver([
+  { isIntersecting },
+]: IntersectionObserverEntry[]) {
+  if (commentStore.isLoading || !commentStore.hasMore || !isIntersecting) return
+  commentStore.fetch()
+}
+
+const isShowMoreComments = ref(false)
 </script>
 
 <template>
-  <div></div>
+  <br />
+  <p>{{ commentStore.total }} comments</p>
+
+  <CommentCard
+    v-for="comment in commentStore.data"
+    :key="comment.id"
+    :message="comment.slug"
+    :date="comment.created_at"
+    :author="comment.name"
+  />
+
+  <UiLoader v-if="pending || commentStore.isLoading" />
+  <div
+    v-if="isShowMoreComments"
+    v-intersection-observer="onIntersectionObserver"
+    class="observer"
+  ></div>
+  <UButton v-else @click="isShowMoreComments = !isShowMoreComments">
+    Раскрыть
+  </UButton>
 </template>
 
 <style lang="scss" scoped></style>
