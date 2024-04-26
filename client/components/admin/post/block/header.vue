a34ff66d6f257f557a0d9b3706da596862b47175
Zendau <cowalyow2012@gmail.com>
2024-04-27 01:05:48 +0300

    feat(client/blockEditor): article header block

---
+++ client/components/admin/post/block/header.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const text = ref("")
+
+function onInput(e: Event) {
+  text.value = (e.target as HTMLInputElement).innerText
+}
+</script>
+
+<template>
+  <!-- Заголовок 1 -->
+  <h1 placeholder="Заголовок 1" contenteditable="true" @input="onInput"></h1>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  font-weight: 500;
+  font-size: 22px;
+  line-height: 30px;
+  padding-top: 6px;
+  padding-bottom: 6px;
+  outline: none;
+}
+</style>
