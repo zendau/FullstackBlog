424cbc7ae4c490ab4bbd294b3c489ea2f303dab8
Zendau <cowalyow2012@gmail.com>
2024-04-27 01:06:41 +0300

    feat(client/blockEditor): article list block

---
+++ client/components/admin/post/block/media/list.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const files = inject<File[]>("files", [])
+
+function removeUploadImg(index: number) {
+  files.splice(index, 1)
+}
+</script>
+
+<template>
+  <AdminPostBlockMediaItem
+    v-for="(file, index) in files"
+    :key="file.name"
+    :file="file"
+    @remove-img="removeUploadImg(index)"
+  />
+</template>
+
+<style lang="scss" scoped></style>
