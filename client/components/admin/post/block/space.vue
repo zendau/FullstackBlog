a8a8f9a7252b690e80b8f591ac04eb4934f77168
Zendau <cowalyow2012@gmail.com>
2024-04-27 01:06:52 +0300

    feat(client/blockEditor): article space block

---
+++ client/components/admin/post/block/space.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <UDivider>
+    <span class="text">***</span>
+  </UDivider>
+</template>
+
+<style lang="scss" scoped>
+.text {
+  width: 100%;
+  text-align: center;
+  font-size: 30px;
+}
+</style>
