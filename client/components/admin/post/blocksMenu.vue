bd9b13acd4bad9e4d6a82a151215b1f559166337
Zendau <cowalyow2012@gmail.com>
2024-04-27 01:18:08 +0300

    feat(client/blocEditor): blocksMenu

---
+++ client/components/admin/post/blocksMenu.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const open = ref(false)
+const { list } = defineProps({
+  list: {
+    type: Array<string>,
+    required: true,
+  },
+})
+
+const emit = defineEmits<{
+  select: [block: string]
+}>()
+
+function selectBlock(block: string) {
+  open.value = false
+  emit("select", block)
+}
+</script>
+
+<template>
+  <UPopover v-model:open="open">
+    <UButton color="white" trailing-icon="i-heroicons-plus-circle-16-solid" />
+
+    <template #panel>
+      <div class="p-4">
+        <h2>Select item</h2>
+        <hr />
+        <p v-for="block in list" :key="block" @click="selectBlock(block)">
+          {{ block }}
+        </p>
+      </div>
+    </template>
+  </UPopover>
+</template>
+
+<style lang="scss" scoped></style>
