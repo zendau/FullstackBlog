e50be2256133410fb0b65f57ac58de53efec0e62
Zendau <cowalyow2012@gmail.com>
2024-04-27 01:06:27 +0300

    feat(client/blockEditor): article text block

---
+++ client/components/admin/post/block/text.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+const text = ref("")
+
+function getData() {
+  return {
+    block: "text",
+    content: text.value,
+  }
+}
+
+defineExpose({
+  getData,
+})
+
+function onKeyDown(event: any) {
+  if (event.code !== "Backspace") {
+    return
+  }
+
+  const targetText = event.target.textContent.trim()
+  if (targetText.length === 0) {
+    event.preventDefault()
+  }
+}
+
+function onKeyUp(event: any) {
+  text.value = event.target.innerHTML
+}
+</script>
+
+<template>
+  <p
+    placeholder="Введите текст"
+    contenteditable="true"
+    @keydown="onKeyDown"
+    @keyup="onKeyUp"
+  ></p>
+</template>
+
+<style lang="scss" scoped></style>
