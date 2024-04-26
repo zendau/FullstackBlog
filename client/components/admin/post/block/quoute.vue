b63d1cf95b857204e02b1005e7b7a8474eb0acb6
Zendau <cowalyow2012@gmail.com>
2024-04-27 01:06:21 +0300

    feat(client/blockEditor): article quoute block

---
+++ client/components/admin/post/block/quoute.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+const text = ref("")
+const author = ref("")
+
+function onInputText(e: Event) {
+  text.value = (e.target as HTMLInputElement).innerText
+}
+
+function onInputAuthor(e: Event) {
+  author.value = (e.target as HTMLInputElement).innerText
+}
+</script>
+
+<template>
+  <blockquote :class="$style.quote">
+    <p
+      :class="$style.text"
+      placeholder="Текст цитаты"
+      contenteditable="true"
+      @input="onInputText"
+    ></p>
+    <p
+      :class="$style.author"
+      placeholder="Подпись"
+      contenteditable="true"
+      @input="onInputAuthor"
+    ></p>
+  </blockquote>
+</template>
+
+<style lang="scss" module>
+.quote {
+  padding: 15px;
+  position: relative;
+}
+
+.quote ::before {
+  content: "";
+  position: absolute;
+  top: 4px;
+  bottom: 4px;
+  left: 0;
+  height: 100%;
+  border-radius: 2px;
+  border-left: 4px solid blue;
+}
+
+.quote .text {
+  font-size: 28px;
+  line-height: 40px;
+}
+
+.quote .author {
+  margin-top: 8px;
+  font-size: 15px;
+  line-height: 22px;
+}
+</style>
