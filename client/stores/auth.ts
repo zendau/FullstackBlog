493846db8c14351a09185d73889ff5f0754fe4bd
Zendau <cowalyow2012@gmail.com>
2024-04-29 21:47:13 +0300

    feat(client): add register and initJWT methods to auth store

--- client/stores/auth.ts
+++ client/stores/auth.ts
@@ -3,48 +3,89 @@ interface IAuthUser {
   password: string
 }
 
+interface IAuthRes {
+  accessToken: string
+  refreshToken: string
+}
+
 export const useAuthStore = defineStore("auth", () => {
   const loadingIndicator = useLoadingIndicator()
   const router = useRouter()
 
-  const accessToken = useCookie("token")
-  const refreshToken = useLocalStorage("token", "")
+  const accessToken = useLocalStorage("token", "", { initOnMounted: true })
+  const refreshToken = useCookie("JWTRefreshToken")
 
   const userStore = useUserStore()
 
   const isLoading = ref(false)
-  const token = ref("")
   const error = ref("")
+
+  const isAuth = ref(false)
 
   async function login(userData: IAuthUser) {
     try {
       isLoading.value = true
       loadingIndicator.start()
-      const res = await $fetch<any>(
-        "https://api.fakestorejson.com/api/v1/auth/login",
-        {
-          method: "post",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            email: userData.email,
-            password: userData.password,
-          }),
-        },
-      )
+      const res = await useApiFetch<IAuthRes>("/user/login", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: userData.email,
+          password: userData.password,
+        }),
+      })
 
-      if (!res.access_token) {
+      const saveStatus = userStore.saveUserData(res.accessToken)
+
+      if (!saveStatus) {
         throw Error
       }
-
-      token.value = res.access_token
-      accessToken.value = token.value
-      refreshToken.value = token.value
-
-      await userStore.getProfile()
 
       await router.push("/")
     } catch (e: any) {
       if (e.status === 401) {
+        error.value =
+          "Пожалуйста, проверьте введенные данные и повторите попытку."
+      } else {
+        error.value =
+          "Произошла непредвиденная ошибка. Пожалуйста, попробуйте еще раз позже."
+      }
+
+      isAuth.value = true
+      setTimeout(() => (error.value = ""), 5000)
+    } finally {
+      loadingIndicator.finish()
+      isLoading.value = false
+    }
+  }
+
+  async function register(userData: IAuthUser) {
+    try {
+      isLoading.value = true
+      loadingIndicator.start()
+      const res = await useApiFetch<IAuthRes>("/user/register", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: userData.email,
+          password: userData.password,
+        }),
+      })
+
+      const saveStatus = userStore.saveUserData(res.accessToken)
+
+      if (!saveStatus) {
+        throw Error
+      }
+
+      accessToken.value = res.accessToken
+      isAuth.value = true
+      await router.push("/")
+    } catch (e: any) {
+      console.log("e", e)
+      if (e.status === 400) {
+        error.value = e.data.message
+      } else if (e.status === 401) {
         error.value =
           "Пожалуйста, проверьте введенные данные и повторите попытку."
       } else {
@@ -59,5 +100,35 @@ export const useAuthStore = defineStore("auth", () => {
     }
   }
 
-  return { isLoading, token, error, accessToken, refreshToken, login }
+  function initJWT() {
+    if (!refreshToken.value) {
+      return false
+    }
+
+    const saveStatus = userStore.saveUserData(refreshToken.value)
+    isAuth.value = true
+    return saveStatus
+  }
+
+  function logout() {
+    isAuth.value = false
+    refreshToken.value = ""
+    accessToken.value = ""
+
+    userStore.$reset()
+    useApiFetch("/user/logout")
+
+    router.push("/")
+  }
+
+  return {
+    error,
+    isAuth,
+    isLoading,
+    accessToken,
+    login,
+    logout,
+    initJWT,
+    register,
+  }
 })
