40d54cd3f4bd7dc5c04f819cb80e17a309769e58
Zendau <cowalyow2012@gmail.com>
2024-04-30 19:57:31 +0300

    feat(client): init settings page

---
+++ client/pages/settings/general.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const userStore = useUserStore()
+</script>
+
+<template>
+  <SettingsUpdateData v-if="userStore.data?.isActivated" />
+  <SettingsStatus v-else />
+</template>
+
+<style lang="scss" scoped></style>
---
+++ client/pages/settings/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <NuxtLink to="/settings/general"> General </NuxtLink>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
