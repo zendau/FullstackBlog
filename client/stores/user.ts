2736915299dcd14f91da82dc80dd18068a8f0e4f
Zendau <cowalyow2012@gmail.com>
2024-04-30 13:15:28 +0300

    feat(client): user store add activateAccount, sendConfirmCode, resetPassword methods

--- client/stores/user.ts
+++ client/stores/user.ts
@@ -25,6 +25,9 @@ export const useUserStore = defineStore("user", () => {
     return data.value.roles.includes(Roles.Admin)
   })
 
+  const isLoading = ref(false)
+  const error = ref("")
+
   function decodeToken(token: string) {
     if (!token) return false
 
@@ -47,9 +50,105 @@ export const useUserStore = defineStore("user", () => {
     return true
   }
 
+  async function activateAccount({ confirmCode }: { confirmCode: string }) {
+    try {
+      error.value = ""
+      isLoading.value = true
+      const res = await useApiFetch<boolean>("/user/activate", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          confirmCode,
+        }),
+      })
+
+      if (!res || !data.value) throw Error
+
+      data.value.isActivated = true
+      return true
+    } catch (e: any) {
+      error.value =
+        "An error occurred when sending the confirmation code. Repeat later."
+
+      setTimeout(() => (error.value = ""), 5000)
+      return false
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  async function sendConfirmCode(email: string | undefined) {
+    try {
+      error.value = ""
+      if (!email) throw new Error("The mail was not entered")
+
+      const res = await useApiFetch<boolean>("/user/sendConfirmCode", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email,
+        }),
+      })
+
+      if (!res) throw Error
+    } catch (e: any) {
+      error.value =
+        e.message ??
+        "An error occurred when receiving a new confirmation code. Repeat later."
+
+      setTimeout(() => (error.value = ""), 5000)
+    }
+  }
+
+  async function resetPassword({
+    email,
+    confirmCode,
+  }: {
+    email: string
+    confirmCode: string
+  }) {
+    try {
+      error.value = ""
+      isLoading.value = true
+      const res = await useApiFetch<{ message: string }>(
+        "/user/resetPassword",
+        {
+          method: "post",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            email,
+            confirmCode,
+          }),
+        },
+      )
+
+      if (!res) throw Error
+      return true
+    } catch (e: any) {
+      error.value =
+        "An error occurred when sending the confirmation code. Repeat later."
+
+      setTimeout(() => (error.value = ""), 5000)
+      return false
+    } finally {
+      isLoading.value = false
+    }
+  }
+
   function $reset() {
     data.value = undefined
   }
 
-  return { data, isAdmin, $reset, decodeToken, saveUserData }
+  return {
+    data,
+    isAdmin,
+    isLoading,
+    error,
+    $reset,
+    decodeToken,
+    saveUserData,
+    resetPassword,
+    activateAccount,
+    sendConfirmCode,
+  }
 })
