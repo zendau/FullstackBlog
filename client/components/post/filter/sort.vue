08f3f65c21fea3be789133c3e7e1a5ba0224c2a6
Zendau <cowalyow2012@gmail.com>
2024-05-03 22:38:22 +0300

    feat(client): sort component

---
+++ client/components/post/filter/sort.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const options = [
+  {
+    value: "byRating",
+    label: "By rating",
+  },
+  {
+    value: "byCreatedDate",
+    label: "By createted date",
+  },
+]
+
+const articleParams = useArticleParamsStore()
+
+function test() {
+  articleParams.addQuery("sort")
+}
+</script>
+
+<template>
+  <URadioGroup
+    v-model="articleParams.sort"
+    legend="Choose sorting type"
+    :options="options"
+    @change="test"
+  />
+</template>
+
+<style lang="scss" scoped></style>
