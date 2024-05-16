80f9297555363e27cac8e2ddbb91501c401b409b
Zendau <cowalyow2012@gmail.com>
2024-05-16 23:09:14 +0300

    feat(client): comments form without uploading files

--- client/components/comment/addForm.vue
+++ client/components/comment/addForm.vue
@@ -1,40 +1,18 @@
 <script setup lang="ts">
-const uploadImgPreviewURL = ref<string>()
-const inputFile = ref<HTMLInputElement>()
+const articleId = inject("articleId", "")
+
 const commentText = ref("")
-const userStore = useUserStore()
 const commentStore = useCommentStore()
-
-function onFIleUpload(e: Event) {
-  const files = (e.target as HTMLInputElement).files
-  if (!files) return
-
-  inputFile.value = e.target as HTMLInputElement
-
-  const src = URL.createObjectURL(files[0])
-
-  uploadImgPreviewURL.value = src
-}
-
-function resetUploadFile() {
-  uploadImgPreviewURL.value = ""
-  if (!inputFile.value) return
-  inputFile.value.value = ""
-}
 
 function onSendMessage() {
   const commentBody = {
-    id: new Date(),
-    slug: commentText.value,
-    name: userStore.data.name,
-    created_at: new Date().toString(),
+    postId: articleId,
+    message: commentText.value,
   }
 
   commentText.value = ""
 
   commentStore.add(commentBody)
-
-  // console.log(commentBody)
 }
 </script>
 
@@ -44,19 +22,11 @@ function onSendMessage() {
       v-model="commentText"
       color="primary"
       variant="outline"
-      placeholder="Search..."
+      placeholder="Comment..."
     />
-    <div class="fileUpload">
-      <label for="file"><UIcon name="i-heroicons-photo-16-solid" /></label>
-      <input id="file" type="file" @change="onFIleUpload" />
-      <div v-if="uploadImgPreviewURL" class="preview-container">
-        <img :src="uploadImgPreviewURL" class="preview-img" />
-        <button class="preview-btn" @click="resetUploadFile">✕</button>
-      </div>
-      <UButton v-if="commentText.length > 0" @click="onSendMessage">
-        send
-      </UButton>
-    </div>
+    <UButton v-if="commentText.length > 0" @click="onSendMessage">
+      Send
+    </UButton>
   </form>
 </template>
 
