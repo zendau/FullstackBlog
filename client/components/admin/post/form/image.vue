33f10a528733b2dcee995e14fe4cf302b68a16c1
Zendau <cowalyow2012@gmail.com>
2024-05-31 23:10:16 +0300

    feat(client): post form. image field

---
+++ client/components/admin/post/form/image.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const files = inject("files", [])
+</script>
+
+<template>
+  <UiFileUpload accept=".jpeg, .jpg, .png, .gif, .svg" />
+
+  <template v-if="files.length > 0">
+    <img :src="getMediaSrc(files[0])" alt="" />
+  </template>
+</template>
+
+<style lang="scss" scoped></style>
