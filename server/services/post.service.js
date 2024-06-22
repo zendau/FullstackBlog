a7401df4d96f2eee9ab8b054b7b1260f5c472140
Zendau <cowalyow2012@gmail.com>
2024-06-22 23:30:41 +0300

    refactor(server): split insert function into smaller functions

--- server/services/post.service.js
+++ server/services/post.service.js
@@ -1,5 +1,12 @@
-import mongoose from "mongoose"
-
+import {
+  blockExtended,
+  postFacet,
+  postLookup,
+  postsExtendedData,
+  postsMatchFilter,
+  postsRating,
+  postsSort,
+} from "../aggregation/pagination.builder.js"
 import ApiError from "../exceprions/api.error.js"
 import {
   CodeBlock,
@@ -33,13 +40,27 @@ class PostService {
 
       return createdPostData
     } catch (e) {
-      if (e instanceof ApiError) {
-        throw e
-      } else {
-        throw new ApiError.InternalError(
-          "Unexpected error when creating an article",
-        )
+      this.handleError(e, "Unexpected error when creating an article")
+    }
+  }
+
+  async edit(author, postData, file) {
+    try {
+      const post = await this.isPostAuthor(author, postData.id)
+
+      if (file) {
+        await FileService.update(post.file, file)
       }
+
+      const updatedPostData = await this.insert("edit", author, {
+        postData,
+        file: post.file,
+        postId: postData.id,
+      })
+
+      return updatedPostData
+    } catch (e) {
+      this.handleError(e, "Unexpected error when editing an article")
     }
   }
 
@@ -47,103 +68,15 @@ class PostService {
     try {
       const tagsList = postData.tags.split(",")
 
-      const postBlocks = []
-
       const blocksData = postData.blocks ? JSON.parse(postData.blocks) : []
 
-      blocksData.forEach((blockData) => {
-        switch (blockData.type) {
-          case "header": {
-            const tempBlock = new HeaderBlock({
-              type: "header",
-              content: blockData.content,
-            })
+      const postBlocks = this.createBlocks(blocksData)
 
-            postBlocks.push(tempBlock)
-
-            break
-          }
-          case "text": {
-            const tempBlock = new TextBlock({
-              type: "text",
-              content: blockData.content,
-            })
-
-            postBlocks.push(tempBlock)
-
-            break
-          }
-          case "quoute": {
-            const tempBlock = new QuoteBlock({
-              type: "quote",
-              content: blockData.content,
-            })
-
-            postBlocks.push(tempBlock)
-
-            break
-          }
-          case "space": {
-            const tempBlock = new SpaceBlock({
-              type: "space",
-            })
-
-            postBlocks.push(tempBlock)
-
-            break
-          }
-          case "code": {
-            const tempBlock = new CodeBlock({
-              type: "code",
-              content: blockData.content,
-            })
-
-            postBlocks.push(tempBlock)
-
-            break
-          }
-          case "list": {
-            const tempBlock = new ListBlock({
-              type: "list",
-              content: blockData.content,
-            })
-
-            postBlocks.push(tempBlock)
-
-            break
-          }
-          case "file": {
-            const tempBlock = new FileBlock({
-              type: "file",
-              list: blockData.content,
-            })
-
-            postBlocks.push(tempBlock)
-
-            break
-          }
-          case "slider": {
-            const tempBlock = new SliderBlock({
-              type: "slider",
-              list: blockData.content,
-            })
-
-            postBlocks.push(tempBlock)
-
-            break
-          }
-          default:
-            break
-        }
-      })
-
-      const preparePostData = {
-        title: postData.title,
-        preview: postData.preview,
-        tags: tagsList,
-        timeRead: postData.timeRead,
-        blocks: postBlocks,
-      }
+      const preparePostData = this.preparePostData(
+        postData,
+        tagsList,
+        postBlocks,
+      )
 
       let post
 
@@ -168,33 +101,119 @@ class PostService {
 
       return createdPostData
     } catch (e) {
-      if (e instanceof ApiError) {
-        throw e
-      } else if (type === "create") {
-        throw new ApiError.InternalError("Error saving the article")
-      } else if (type === "edit") {
-        throw new ApiError.InternalError("Error updating the article")
-      } else {
-        throw new ApiError.UnexpectedError()
-      }
+      this.handleInsertError(e)
     }
   }
 
-  async edit(author, postData, file) {
-    try {
-      const post = await this.isPostAuthor(author, postData.id)
+  createBlocks(blocksData) {
+    return blocksData.map((blockData) => {
+      switch (blockData.type) {
+        case "header": {
+          const tempBlock = new HeaderBlock({
+            type: "header",
+            content: blockData.content,
+          })
 
-      if (file) {
-        await FileService.update(post.file, file)
+          return tempBlock
+        }
+        case "text": {
+          const tempBlock = new TextBlock({
+            type: "text",
+            content: blockData.content,
+          })
+
+          return tempBlock
+        }
+        case "quoute": {
+          const tempBlock = new QuoteBlock({
+            type: "quote",
+            content: blockData.content,
+          })
+
+          return tempBlock
+        }
+        case "space": {
+          const tempBlock = new SpaceBlock({
+            type: "space",
+          })
+
+          return tempBlock
+        }
+        case "code": {
+          const tempBlock = new CodeBlock({
+            type: "code",
+            content: blockData.content,
+          })
+
+          return tempBlock
+        }
+        case "list": {
+          const tempBlock = new ListBlock({
+            type: "list",
+            content: blockData.content,
+          })
+
+          return tempBlock
+        }
+        case "file": {
+          const tempBlock = new FileBlock({
+            type: "file",
+            list: blockData.content,
+          })
+
+          return tempBlock
+        }
+        case "slider": {
+          const tempBlock = new SliderBlock({
+            type: "slider",
+            list: blockData.content,
+          })
+
+          return tempBlock
+        }
+        default:
+          throw new ApiError.InternalError(
+            `unknown block type - ${blockData.type}`,
+          )
       }
+    })
+  }
 
-      const updatedPostData = await this.insert("edit", author, {
-        postData,
-        file: post.file,
-        postId: postData.id,
-      })
+  preparePostData(postData, tagsList, postBlocks) {
+    return {
+      title: postData.title,
+      preview: postData.preview,
+      tags: tagsList,
+      timeRead: postData.timeRead,
+      blocks: postBlocks,
+    }
+  }
 
-      return updatedPostData
+  handleInsertError(e, type) {
+    if (e instanceof ApiError) {
+      throw e
+    } else if (type === "create") {
+      throw new ApiError.InternalError("Error saving the article")
+    } else if (type === "edit") {
+      throw new ApiError.InternalError("Error updating the article")
+    } else {
+      throw new ApiError.UnexpectedError()
+    }
+  }
+
+  handleError(e, defaultMessage) {
+    if (e instanceof ApiError) {
+      throw e
+    } else {
+      throw new ApiError.InternalError(defaultMessage)
+    }
+  }
+
+  async delete(postId, userId) {
+    try {
+      const post = await this.isPostAuthor(userId, postId)
+      post.deleteOne()
+      await ReactionService.deletePostReactions(postId)
     } catch (e) {
       if (e instanceof ApiError) {
         throw e
@@ -204,14 +223,6 @@ class PostService {
         )
       }
     }
-  }
-
-  async delete(postId, userId) {
-    const postData = await this.postExist(postId)
-    this.checkPostAuthor(userId, postData.author.id)
-
-    postData.deleteOne()
-    await ReactionService.deletePostReactions(postId)
 
     return true
   }
@@ -227,15 +238,15 @@ class PostService {
   }
 
   async getOne(postId, ip) {
-    const filter = this.postsMatchFilter(null, { postId })
-    const postLookup = this.postLookup()
-    const rating = this.postsRating(true)
-    const postExtended = this.postsExtendedData()
-    const blocksExntended = this.blockExtended()
+    const filter = postsMatchFilter(null, { postId })
+    const postLookupData = postLookup()
+    const rating = postsRating(true)
+    const postExtended = postsExtendedData()
+    const blocksExntended = blockExtended()
 
     const combineAggregate = [
       ...filter,
-      ...postLookup,
+      ...postLookupData,
       ...rating,
       ...postExtended,
       ...blocksExntended,
@@ -277,448 +288,18 @@ class PostService {
     return true
   }
 
-  postLookup() {
-    return [
-      {
-        $lookup: {
-          from: "comments",
-          localField: "_id",
-          foreignField: "post",
-          as: "comments",
-        },
-      },
-      {
-        $lookup: {
-          from: "userpostreads",
-          localField: "_id",
-          foreignField: "post",
-          as: "postreads",
-        },
-      },
-      {
-        $lookup: {
-          from: "reactions",
-          localField: "_id",
-          foreignField: "post",
-          as: "react",
-        },
-      },
-    ]
-  }
-
-  postsRating(withCounters) {
-    return [
-      {
-        $project: {
-          _id: 1,
-          comments: {
-            $size: "$comments",
-          },
-          counterReads: {
-            $size: "$postreads",
-          },
-          react: "$react",
-        },
-      },
-      {
-        $unwind: {
-          path: "$react",
-          preserveNullAndEmptyArrays: true,
-        },
-      },
-      {
-        $group: {
-          _id: "$_id",
-          counterLikes: {
-            $sum: {
-              $cond: {
-                if: {
-                  $eq: ["$react.isLiked", true],
-                },
-                then: 1,
-                else: 0,
-              },
-            },
-          },
-          counterDislikes: {
-            $sum: {
-              $cond: {
-                if: {
-                  $eq: ["$react.isLiked", false],
-                },
-                then: 1,
-                else: 0,
-              },
-            },
-          },
-          counterComments: {
-            $first: "$comments",
-          },
-          counterReads: {
-            $first: "$counterReads",
-          },
-        },
-      },
-      {
-        $project: {
-          rating: {
-            $sum: [
-              {
-                $multiply: ["$counterLikes", 2],
-              },
-              {
-                $multiply: ["$counterDislikes", -1],
-              },
-              {
-                $multiply: ["$counterComments", 3],
-              },
-              {
-                $multiply: ["$counterReads", 0.1],
-              },
-            ],
-          },
-          ...(withCounters && {
-            counterReads: 1,
-            counterComments: 1,
-            counterLikes: 1,
-            counterDislikes: 1,
-          }),
-        },
-      },
-    ]
-  }
-
-  postsMatchFilter(idList, filter) {
-    const matchData = {}
-
-    const objectIdList = Array.isArray(idList)
-      ? idList.map((id) => mongoose.Types.ObjectId(id))
-      : []
-
-    // Выборка постов конкретного пользователя
-    if (filter?.authorId) {
-      matchData.author = {
-        $eq: mongoose.Types.ObjectId(filter.authorId),
-      }
-    }
-
-    // Выборка постов по конкретному тегу
-    if (filter?.tag) {
-      matchData.tags = {
-        $elemMatch: { $eq: filter.tag },
-      }
-    }
-
-    // Выборка постов по подстроке
-    if (filter.substring) {
-      const regex = new RegExp(filter.substring, "i")
-
-      matchData.title = {
-        $regex: regex,
-      }
-    }
-
-    // Получение поста по айди
-    if (filter.postId) {
-      matchData._id = {
-        $eq: mongoose.Types.ObjectId(filter.postId),
-      }
-    }
-
-    if (objectIdList.length > 0) {
-      // Исключения постов которые уже были получины
-      matchData._id = {
-        $nin: objectIdList,
-      }
-    }
-
-    return [
-      {
-        $match: {
-          ...matchData,
-        },
-      },
-    ]
-  }
-
-  postsExtendedData() {
-    return [
-      {
-        $lookup: {
-          from: "posts",
-          localField: "_id",
-          foreignField: "_id",
-          as: "post",
-        },
-      },
-      {
-        $unwind: "$post",
-      },
-      {
-        $lookup: {
-          from: "files",
-          localField: "post.file",
-          foreignField: "_id",
-          as: "file",
-        },
-      },
-      {
-        $unwind: "$file",
-      },
-      {
-        $lookup: {
-          from: "users",
-          localField: "post.author",
-          foreignField: "_id",
-          as: "author",
-        },
-      },
-      {
-        $unwind: "$author",
-      },
-      {
-        $project: {
-          _id: 0,
-          id: "$_id",
-          preview: "$post.preview",
-          tags: "$post.tags",
-          author: {
-            id: "$author._id",
-            email: 1,
-          },
-          title: "$post.title",
-          blocks: "$post.blocks",
-          timeRead: "$post.timeRead",
-          file: {
-            id: "$file._id",
-            fileName: 1,
-            size: 1,
-            mimetype: 1,
-          },
-          createdDate: "$post.createdDate",
-          rating: {
-            $sum: [
-              {
-                $multiply: ["$counterLikes", 2],
-              },
-              {
-                $multiply: ["$counterDislikes", -1],
-              },
-              {
-                $multiply: ["$counterComments", 3],
-              },
-              {
-                $multiply: ["$counterReads", 0.1],
-              },
-            ],
-          },
-
-          counterReads: 1,
-          counterComments: 1,
-          counterLikes: 1,
-          counterDislikes: 1,
-        },
-      },
-    ]
-  }
-
-  postsSort(filter) {
-    const sort = {}
-
-    switch (filter) {
-      case "byCreatedDate":
-        sort.createdDate = -1
-        break
-
-      case "byRating":
-      default:
-        sort.rating = -1
-        break
-    }
-
-    return [
-      {
-        $sort: sort,
-      },
-    ]
-  }
-
-  blockExtended() {
-    return [
-      {
-        $unwind: "$blocks",
-      },
-      {
-        $lookup: {
-          from: "files",
-          localField: "blocks.list",
-          foreignField: "_id",
-          as: "blocks.list",
-        },
-      },
-      {
-        $addFields: {
-          blocks: {
-            _id: "$$REMOVE",
-            list: {
-              $map: {
-                input: "$blocks.list",
-                as: "item",
-                in: {
-                  id: "$$item._id",
-                  fileName: "$$item.fileName",
-                  size: "$$item.size",
-                  mimetype: "$$item.mimetype",
-                },
-              },
-            },
-          },
-        },
-      },
-      {
-        $addFields: {
-          blocks: {
-            list: {
-              $cond: {
-                if: {
-                  $eq: ["$blocks.list", []],
-                },
-                then: "$$REMOVE",
-                else: "$blocks.list",
-              },
-            },
-          },
-        },
-      },
-      {
-        $set: {
-          blocks: {
-            $cond: {
-              if: {
-                $and: [
-                  {
-                    $not: ["$blocks.content"],
-                  },
-                  "$blocks.list",
-                ],
-              },
-              then: {
-                $mergeObjects: [
-                  "$blocks",
-                  {
-                    content: "$blocks.list",
-                  },
-                ],
-              },
-              else: "$blocks",
-            },
-          },
-        },
-      },
-      {
-        $unset: "blocks.list",
-      },
-      {
-        $group: {
-          _id: "$id",
-          doc: {
-            $first: "$$ROOT",
-          },
-          blocks: {
-            $push: "$blocks",
-          },
-        },
-      },
-      {
-        $replaceRoot: {
-          newRoot: {
-            $mergeObjects: ["$doc", "$$ROOT"],
-          },
-        },
-      },
-      {
-        $project: {
-          _id: 0,
-          doc: 0,
-        },
-      },
-      // {
-      //   $project: {
-      //     id: "$_id",
-      //     blocks: 1,
-      //     tags: "$doc.tags",
-      //     author: "$doc.author",
-      //     title: "$doc.title",
-      //     body: "$doc.body",
-      //     timeRead: "$doc.timeRead",
-      //     createdDate: "$doc.createdDate",
-      //     test: "$doc",
-      //   },
-      // },
-    ]
-  }
-
-  postFacet(skip, limit, sort) {
-    const postsLimit = { $limit: limit }
-    const postsSkip = { $skip: skip }
-
-    const facetPosts = []
-
-    if (skip) {
-      facetPosts.push(postsSkip)
-    }
-    if (limit >= 0) {
-      facetPosts.push(postsLimit)
-    }
-
-    const facet = [
-      {
-        $facet: {
-          posts: [...sort, ...facetPosts],
-          total: [
-            {
-              $group: {
-                _id: null,
-                count: {
-                  $sum: 1,
-                },
-              },
-            },
-            {
-              $project: {
-                _id: 0,
-                count: 1,
-              },
-            },
-          ],
-        },
-      },
-      {
-        $unwind: "$total",
-      },
-      {
-        $project: {
-          posts: 1,
-          total: "$total.count",
-        },
-      },
-    ]
-
-    return facet
-  }
-
   async getPostsPagination(idList, limit, skip, sortType, filterType) {
-    const filter = this.postsMatchFilter(idList, filterType)
-    const postLookup = this.postLookup()
-    const rating = this.postsRating(true)
-    const extended = this.postsExtendedData()
-    const sort = this.postsSort(sortType)
+    const filter = postsMatchFilter(idList, filterType)
+    const postLookupData = postLookup()
+    const rating = postsRating(true)
+    const extended = postsExtendedData()
+    const sort = postsSort(sortType)
 
-    const facet = this.postFacet(skip, limit, sort)
+    const facet = postFacet(skip, limit, sort)
 
     const combineAggregate = [
       ...filter,
-      ...postLookup,
+      ...postLookupData,
       ...rating,
       ...extended,
       ...facet,
