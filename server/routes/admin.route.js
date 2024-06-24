82b81f9d019d1c3009ba54b2f35731405ff6de13
Zendau <cowalyow2012@gmail.com>
2024-06-24 19:35:45 +0300

    refactor(server): delete deprecated get all users method

--- server/routes/admin.route.js
+++ server/routes/admin.route.js
@@ -1,37 +1,9 @@
 import { Router } from "express"
 
 import AdminController from "../controllers/admin.controller.js"
-import UserController from "../controllers/user.controller.js"
 import { adminGuard } from "../middlewares/auth.middleware.js"
 
 const router = new Router()
-
-/**
- * @swagger
- * /user/all:
- *   get:
- *     summary: Get all users
- *     tags: [User]
- *     security:
- *      - bearerAuth: []
- *     responses:
- *       200:
- *         description: Array of users
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                $ref: '#/components/DTOs/User'
- *       400:
- *          description: Error message
- *       401:
- *         description: User is not auth
- *       500:
- *         description: Unexpected error
- */
-
-router.get("/all", adminGuard, UserController.userList)
 
 router.patch("/roles", adminGuard, AdminController.getRoles)
 router.patch("/setRole", adminGuard, AdminController.setUserRole)
