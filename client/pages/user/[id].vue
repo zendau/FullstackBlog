19a95e28fb9f583a75f9e32f0357daeaf11f1670
Zendau <cowalyow2012@gmail.com>
2024-07-01 21:44:34 +0300

    style(client): acount page

--- client/pages/user/[id].vue
+++ client/pages/user/[id].vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
+interface IUser {
+  counterLikes: number
+  counterDislikes: number
+  rating: number
+  id: string
+  isBlocked: boolean
+  email: string
+  counterPosts: number
+  counterComments: number
+}
+
 const { params } = useRoute()
 const userId = params.id as string
 
 provide("userId", userId)
 
-const { data, pending } = await useAsyncData(
+const { data, pending } = useLazyAsyncData<IUser>(
   `user/${userId}`,
   async () => await useApiFetch(`user/data/${userId}`),
   {
@@ -14,17 +25,41 @@ const { data, pending } = await useAsyncData(
 </script>
 
 <template>
-  {{ pending }}
-  <div>{{ data }}</div>
+  <div v-if="pending"><UiLoader /></div>
+  <template v-else>
+    <div v-if="!data">not found</div>
 
-  <UiTabView>
-    <UiTabItem title="Posts">
-      <UserPostList />
-    </UiTabItem>
-    <UiTabItem title="Comments">
-      <UserCommentList />
-    </UiTabItem>
-  </UiTabView>
+    <div v-else class="mt-4">
+      <h1 class="text-xl font-medium">{{ data.email }}</h1>
+      <p class="text-sm">
+        {{ data.isBlocked ? "Blocked" : "Not blocked" }} account status
+      </p>
+      <p class="flex items-center my-3">
+        <UIcon name="i-heroicons-trophy mr-2 text-3xl" />
+        {{ data.rating }}
+      </p>
+
+      <div class="flex my-3">
+        <p class="flex items-center mr-4">
+          <UIcon name="i-iconamoon-like" class="text-3xl mr-2" />
+          {{ data.counterLikes }}
+        </p>
+        <p class="flex items-center mr-4">
+          <UIcon name="i-iconamoon-dislike" class="text-3xl mr-2" />{{
+            data.counterDislikes
+          }}
+        </p>
+      </div>
+      <UiTabView>
+        <UiTabItem :title="`Articles ${data.counterPosts}`">
+          <UserPostList />
+        </UiTabItem>
+        <UiTabItem :title="`Comments ${data.counterComments}`">
+          <UserCommentList />
+        </UiTabItem>
+      </UiTabView>
+    </div>
+  </template>
 </template>
 
 <style lang="scss" scoped></style>
