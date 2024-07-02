ad62206360ec79bdef09251bea0d1a5b3a4d4c0e
Zendau <cowalyow2012@gmail.com>
2024-07-02 18:10:40 +0300

    feat(client): view container component for create, edit article form

---
+++ client/components/admin/post/form/viewContainer.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const { error } = defineProps<{
+  error?: Error | null
+}>()
+</script>
+
+<template>
+  <div class="view-container">
+    <h1><slot name="header" /></h1>
+    <UAlert
+      v-if="error"
+      icon="i-heroicons-command-line"
+      title="Error"
+      color="red"
+      description="Article not found"
+      class="my-3"
+    />
+    <slot v-else name="body" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.view-container {
+  position: relative;
+  margin: 15px 0;
+  background-color: #fff;
+  padding: 20px;
+}
+
+h1 {
+  font-size: 22px;
+  font-weight: 500;
+  text-align: center;
+}
+</style>
