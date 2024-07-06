c3d4a5f1d8efb5b34e729e74de229300396a1778
Zendau <cowalyow2012@gmail.com>
2024-07-06 16:40:37 +0300

    feat(client/admin/users): roles list

---
+++ client/components/admin/users/role/item.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { Roles } from "../../../../stores/user"
+
+const { role } = defineProps<{
+  role: Roles
+}>()
+</script>
+
+<template>
+  <li>
+    {{ role }}
+  </li>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ client/components/admin/users/role/list.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { Roles } from "../../../../stores/user"
+
+const { roles } = defineProps<{
+  roles: Roles[]
+}>()
+</script>
+
+<template>
+  <ul>
+    <AdminUsersRoleItem v-for="role in roles" :key="role" :role="role" />
+  </ul>
+</template>
+
+<style lang="scss" scoped></style>
