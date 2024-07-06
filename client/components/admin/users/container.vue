fec8054df4d97115fc23003778869598bbe7226b
Zendau <cowalyow2012@gmail.com>
2024-07-06 16:47:55 +0300

    feat(client/admin): users page and container component

---
+++ client/components/admin/users/container.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const adminStore = useAdminStore()
+
+function onPaginationChange(page: number) {
+  adminStore.page = page
+  adminStore.fetch({ isRewrite: true, params: {} })
+}
+</script>
+
+<template>
+  <div class="p-3">
+    <div class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
+      <AdminUsersSearch />
+    </div>
+    <AdminUsersTable />
+    <div
+      class="flex justify-end px-3 py-3.5 border-t border-gray-200 dark:border-gray-700"
+    >
+      <UPagination
+        v-model="adminStore.page"
+        :page-count="adminStore.count"
+        :total="adminStore.total"
+        show-last
+        show-first
+        @update:model-value="onPaginationChange"
+      />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
--- client/pages/admin/users.vue
+++ client/pages/admin/users.vue
@@ -7,10 +7,17 @@ definePageMeta({
 useHead({
   title: "Site users",
 })
+
+const adminStore = useAdminStore()
+
+useLazyAsyncData("users", () =>
+  adminStore.fetch({ isRewrite: true, params: {} }),
+)
 </script>
 
 <template>
-  <h1>Users list</h1>
+  <h1 class="text-center text-xl font-medium my-4">Users list</h1>
+  <AdminUsersContainer />
 </template>
 
 <style scoped lang="scss"></style>
