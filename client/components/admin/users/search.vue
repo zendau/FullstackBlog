f96a35c49091004e6dfb8d6dd79f32a912f35078
Zendau <cowalyow2012@gmail.com>
2024-07-06 16:47:25 +0300

    feat(client/admin/users): search table  component

---
+++ client/components/admin/users/search.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const adminStore = useAdminStore()
+
+const searchValue = ref("")
+
+const onInputHandler = useDebounceFn(function onInputChange(event: any) {
+  const searchValue = event.target.value
+
+  if (searchValue.length) {
+    adminStore.fetch({
+      isRewrite: true,
+      params: { substring: event.target.value },
+    })
+  } else {
+    adminStore.fetch({ isRewrite: true, params: {} })
+  }
+})
+</script>
+
+<template>
+  <UInput
+    v-model="searchValue"
+    placeholder="Filter people..."
+    @input="onInputHandler"
+  />
+</template>
+
+<style lang="scss" scoped></style>
